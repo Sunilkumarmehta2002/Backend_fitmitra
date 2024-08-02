3279b1bc7e3e7d9e18efa704289076f1acda8138
Sunilkumarmehta2002 <mehtasunilkumar69@gmail.com>
2024-08-02 21:37:18 +0530

    2c

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import app from "./app.js"; 
  // Load environment variables
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT ;
 const baseUrl = process.env.BASE_URL;
 
-app.listen(port, () => {
+app.listen(baseurl, () => {
   console.log(`Server is running on backend url `);
 });
